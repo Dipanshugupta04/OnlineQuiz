b6cc80aea9d5e3285cd9ce1396003156d1f70f7c
Dipanshu <dipanshugupta9462@gmail.com>
2025-03-23 00:08:26 +0530

    update repo

--- src/main/java/com/OnlineQuiz/OnlineQuiz/Reposistory/QuestionRepo.java
+++ src/main/java/com/OnlineQuiz/OnlineQuiz/Reposistory/QuestionRepo.java
@@ -1,9 +1,10 @@
 package com.OnlineQuiz.OnlineQuiz.Reposistory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.OnlineQuiz.OnlineQuiz.Entity.Question;
-
+@Repository
 public interface QuestionRepo extends JpaRepository<Question,Long> {
     
 }
--- src/main/java/com/OnlineQuiz/OnlineQuiz/Reposistory/QuizRepo.java
+++ src/main/java/com/OnlineQuiz/OnlineQuiz/Reposistory/QuizRepo.java
@@ -1,9 +1,10 @@
 package com.OnlineQuiz.OnlineQuiz.Reposistory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.OnlineQuiz.OnlineQuiz.Entity.Quiz;
-
+@Repository
 public interface QuizRepo extends JpaRepository<Quiz,Long>  {
     
 }
