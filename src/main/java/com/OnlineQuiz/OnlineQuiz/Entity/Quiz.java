27e30be4e86f2441443f20a0daf64765089d76fe
Dipanshu <dipanshugupta9462@gmail.com>
2025-03-22 23:58:51 +0530

    update the Quiz Entity

--- src/main/java/com/OnlineQuiz/OnlineQuiz/Entity/Quiz.java
+++ src/main/java/com/OnlineQuiz/OnlineQuiz/Entity/Quiz.java
@@ -1,9 +1,14 @@
 package com.OnlineQuiz.OnlineQuiz.Entity;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,8 +23,73 @@ public class Quiz {
     
     private String title; // Quiz Paper Name
 
+    private String UserName;
+
+    //  Automatically time creation
+
+     @CreationTimestamp
+    @Column(updatable = false)  // Prevent updates to this field
+    private LocalDateTime createdAt;
+
+//  Automatically time creation
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
     @OneToMany(mappedBy = "quiz", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Question> questions = new ArrayList<>();
 
+
+
     // Getters and Setters
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<Question> questions) {
+        this.questions = questions;
+    }
+
+    
 }
