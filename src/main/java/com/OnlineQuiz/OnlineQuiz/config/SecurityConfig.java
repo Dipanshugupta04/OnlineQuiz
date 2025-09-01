bc6c54a9bf9f0d2db1d0ee63e2e5444f1e6599b8
Dipanshu <dipanshugupta9462@gmail.com>
2025-09-01 20:29:17 +0530

    docker file creation

--- src/main/java/com/OnlineQuiz/OnlineQuiz/config/SecurityConfig.java
+++ src/main/java/com/OnlineQuiz/OnlineQuiz/config/SecurityConfig.java
@@ -136,7 +136,7 @@ public class SecurityConfig {
         CorsConfigurationSource corsConfigurationSource() {
                 CorsConfiguration configuration = new CorsConfiguration();
                 configuration.setAllowedOrigins(List.of(
-                                "https://majestic-kangaroo-33ba55.netlify.app",
+                                "onlinequizwin.netlify.app",
                                 "http://127.0.0.1:5502",
                                 "http://localhost:5502",
                                 "http://127.0.0.1:5504",
